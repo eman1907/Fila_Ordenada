3987b6d1aff6d49c398d13201c305a9a0d0c85fe
eman1907 <emanuel.moraes@icomp.ufam.edu.br>
2024-11-27 19:48:26 -0400

    Update Fila.h

--- Fila.h
+++ Fila.h
@@ -18,6 +18,13 @@ void fila_insere(Fila* f, int v);
 
 void fila_ordena(Fila *f); 
 
+int verifica_ordem_certa(Fila *f); 
+
+int verifica_ordem_errada(Fila *f); 
+
+void inverte_fila(Fila* f);
+
 int fila_remove(Fila* f); 
 
 void fila_libera(Fila *f);
+
